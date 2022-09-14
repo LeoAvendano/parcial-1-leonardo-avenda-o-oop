9d0d0d66e2754a9495aa72823d0331cbd74523a2
LeoAvendano <109985336+LeoAvendano@users.noreply.github.com>
2022-09-14 16:19:26 -0500

    Create code_to_run.py

---
+++ code_to_run.py
@@ -0,0 +1,57 @@
+# parcial-1-leonardo-avenda-o-oop
+Elija una opción
+
+1.- Agregar
+2.- Editar
+3.- Listar
+4.- Eliminar
+5.- Salir
+
+?: agegar
+Opción no válida
+
+Elija una opción
+
+1.- Agregar
+2.- Editar
+3.- Listar
+4.- Eliminar
+5.- Salir
+
+?: 1
+
+Titulo: montañas
+Autor: leonardo
+Editorial: jajaj
+
+Elija una opción
+
+1.- Agregar
+2.- Editar
+3.- Listar
+4.- Eliminar
+5.- Salir
+
+?: 
+Opción no válida
+
+Elija una opción
+
+1.- Agregar
+2.- Editar
+3.- Listar
+4.- Eliminar
+5.- Salir
+
+?: salir
+Opción no válida
+
+Elija una opción
+
+1.- Agregar
+2.- Editar
+3.- Listar
+4.- Eliminar
+5.- Salir
+
+?: 
